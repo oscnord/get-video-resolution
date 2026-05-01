40155e49fef92f15265871d1d85435062e4e0a1c
Oscar Nord <oscar.nord@gmail.com>
2026-05-01 13:09:24 +0200

    style: order type exports before value exports per biome

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,13 @@ import {
 } from "./errors";
 import { getVideoResolution } from "./resolver";
 
+export type {
+  AudioTrack,
+  GetVideoResolutionOptions,
+  Resolution,
+  SubtitleTrack,
+  VideoInfo,
+} from "./types";
 export {
   getVideoResolution,
   ManifestParseError,
@@ -15,10 +22,3 @@ export {
   UnsupportedSourceError,
   VideoResolutionError,
 };
-export type {
-  AudioTrack,
-  GetVideoResolutionOptions,
-  Resolution,
-  SubtitleTrack,
-  VideoInfo,
-} from "./types";
